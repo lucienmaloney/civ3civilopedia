34d6c7f328d55500fbeccb71eb750cd86557c638
Lucien Maloney <hobbesdevelopment@gmail.com>
2019-06-08 14:23:40 -0500

    Progress on menu

--- app.js
+++ app.js
@@ -14,51 +14,50 @@ app.get('/', function(req, res) {
   res.send('Home page');
 });
 
-const VIEWS = {
-  bldg: 'improvement',
-  tech: 'advances',
-  prto: 'unit',
-  good: 'resources',
-  gvmt: 'government',
-  terr: 'terrain',
-  tfrm: 'actions',
-  race: 'concepts',
-  gcon: 'concepts',
-  menu: 'concepts',
-};
+class Page {
+  constructor(view, name, image, abbr) {
+    this.view = view;
+    this.name = name;
+    this.image = image;
+    this.abbr = abbr;
+  }
 
-const NAMES = {
-  race: 'Tribes',
-  tech: 'Civilization Advances',
-  gwdr: 'Great Wonders',
-  swdr: 'Small Wonders',
-  bldg: 'City Improvements',
-  gvmt: 'Governments',
-  prto: 'Units',
-  tfrm: 'Worker Actions',
-  terr: 'Terrain',
-  good: 'Resources',
-  gcon: 'Game Concepts',
-};
+  get link() {
+    return `<a href="/civilopedia/${this.abbr}">${this.name}</a>`;
+  }
+}
+
+const PAGES = {
+  bldg: new Page('improvement', 'City Improvements', 'citywallsmea', 'bldg'),
+  tech: new Page('advances', 'Civiliation Advances', 'literature', 'tech'),
+  prto: new Page('unit', 'Units', 'rifleman', 'prto', 'prto'),
+  good: new Page('resources', 'Resources', 'oil', 'good'),
+  gvmt: new Page('government', 'Governments', 'democracy', 'gvmt'),
+  terr: new Page('terrain', 'Terrain', 'mountains', 'terr'),
+  tfrm: new Page('actions', 'Worker Actions', 'clearwetlands', 'tfrm'),
+  race: new Page('concepts', 'Tribes', 'menu', 'race'),
+  gcon: new Page('concepts', 'Game Concepts', 'concepts', 'gcon'),
+  gwdr: new Page('improvement', 'Great Wonders', 'pyramid', 'gwdr'),
+  swdr: new Page('improvement', 'Small Wonders', 'apollo', 'swdr'),
+}
 
 const CIVILOPEDIA_JSON = require('./public/civ3complete.json');
 
 app.get('/civilopedia', function(req, res, next) {
-  const keys = Object.keys(NAMES);
-  let links = keys.map(k => {
-    return `<a href="/civilopedia/${k}">${NAMES[k]}</a>`;
-  });
+  const keys = Object.keys(PAGES);
 
-  res.status(200).render(VIEWS.gcon, {
-    text: links.join('\n'),
+  res.status(200).render(PAGES.gcon.view, {
+    text: '',
     header: 'Main Menu',
+    image: 'menuredlarge',
+    menu: keys.map(k => PAGES[k]),
   });
 });
 
 app.get('/civilopedia/:section', function(req, res, next) {
   const section = (req.params.section || '').toLowerCase();
 
-  if (NAMES[section]) {
+  if (PAGES[section].name) {
     let keys = Object.keys(CIVILOPEDIA_JSON[section] || CIVILOPEDIA_JSON.bldg);
     if (section === 'bldg') {
       keys = keys.filter(k => CIVILOPEDIA_JSON.bldg[k].type === 'Improvement');
@@ -73,9 +72,11 @@ app.get('/civilopedia/:section', function(req, res, next) {
       return `<a href="${Civ.fileNameToUrlPath(k)}">${(CIVILOPEDIA_JSON[section] || CIVILOPEDIA_JSON.bldg)[k].name}</a>`
     });
 
-    res.status(200).render(VIEWS.menu, {
-      header: NAMES[section],
+    res.status(200).render(PAGES.gcon.view, {
+      header: PAGES[section].name,
       text: links.join('\n'),
+      image: PAGES[section].image + 'large',
+      menu: [],
     });
   } else {
     next();
@@ -90,9 +91,11 @@ app.get('/civilopedia/:section/:page/:desc(desc)?', function(req, res, next) {
   if (CIVILOPEDIA_JSON[section] && CIVILOPEDIA_JSON[section][full]) {
     const data = CIVILOPEDIA_JSON[section][full];
 
-    res.status(200).render(VIEWS[section], {
+    res.status(200).render(PAGES[section].view, {
       text: Civ.parseText(data.text),
       header: data.name,
+      image: `${page.toLowerCase().replace(/_/g, '')}large`,
+      menu: [],
     });
   } else {
     next();
