2b06d1e88c8aaa9094ddcf90e018a4ed5b43a226
Lucien Maloney <hobbesdevelopment@gmail.com>
2019-11-01 09:15:02 -0500

    Added interface for map revealer page

--- routes/utilroutes.js
+++ routes/utilroutes.js
@@ -5,4 +5,8 @@ router.get('/civ3decompressor', function(req, res) {
   res.status(200).render('utils/civ3decompressor');
 });
 
+router.get('/civ3maprevealer', function(req, res) {
+  res.status(200).render('utils/civ3maprevealer');
+});
+
 module.exports = router;
